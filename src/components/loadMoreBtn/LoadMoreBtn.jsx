bc42e217c59ca860889ba08b9fdcb7f62d1d19cf
tapsikk <tapsgame@gmail.com>
2024-09-24 17:01:32 +0300

    fix style

--- src/components/loadMoreBtn/LoadMoreBtn.jsx
+++ src/components/loadMoreBtn/LoadMoreBtn.jsx
@@ -1,8 +1,9 @@
 import { Button } from "react-bootstrap";
 
+
 const LoadMoreBtn = ({ onClick }) => {
   return (
-    <Button className="buttons" onClick={onClick}>
+    <Button className='buttons' onClick={onClick}>
       Load more
     </Button>
   );
