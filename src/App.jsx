8adeaaeeea6cd8d04142762420f39167e6c6c8b0
tapsikk <tapsgame@gmail.com>
2024-10-02 12:28:49 +0300

    fix and new loader.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import ErrorMessage from "./components/errorMessage/ErrorMessage";
 import LoadMoreBtn from "./components/loadMoreBtn/LoadMoreBtn";
-import { BallTriangle } from "react-loader-spinner";
+// import { BallTriangle } from "react-loader-spinner";
+import { MutatingDots } from 'react-loader-spinner';
 import ImageModal from "./components/imageModal/ImageModal";
 import ImageGallery from "./components/imageGallery/ImageGallery";
 import SearchBar from "./components/searchBar/SearchBar";
@@ -15,7 +16,7 @@ const FILTERS = ["nature", "birds", "cats", "shoes"];
 function App() {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);  // Добавлено состояние для totalPages
+  const [totalPages, setTotalPages] = useState(0);
   const [errorMsg, setErrorMsg] = useState("");
   const [loading, setLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
@@ -44,7 +45,7 @@ function App() {
         setErrorMsg("No images found for your query.");
       } else {
         setImages((prevImages) => [...prevImages, ...data.results]);
-        setTotalPages(data.total_pages);  // Устанавливаем общее количество страниц
+        setTotalPages(data.total_pages);
       }
     } catch (error) {
       setErrorMsg("Error fetching images. Try again later.");
@@ -76,12 +77,20 @@ function App() {
           </div>
         ))}
       </div>
-        {errorMsg && <ErrorMessage className="errorMsgContainer" message={errorMsg} />}
-      {loading && <BallTriangle color="grey" height={100} width={100} />}
+      {errorMsg && <ErrorMessage className="errorMsgContainer" message={errorMsg} />}
+      
+      {loading && (
+        <div className="loader-container">
+          <MutatingDots color='#7676d7' secondaryColor='#7676d7' height={100} width={100} />
+        </div>
+      )}
+      
       <ImageGallery images={images} openModal={openImageModal} />
+      
       {!loading && images.length > 0 && page < totalPages && (
         <LoadMoreBtn onClick={loadMoreImages} />
       )}
+      
       {selectedImageUrl && (
         <ImageModal imageUrl={selectedImageUrl} closeModal={closeImageModal} />
       )}
