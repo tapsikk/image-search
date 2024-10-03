a8cfc5364d7c2e417d0fd79824c70614a85c6dd2
tapsikk <tapsgame@gmail.com>
2024-10-03 21:33:06 +0300

    mini-animation for btn "load more"

--- src/components/loadMoreBtn/LoadMoreBtn.jsx
+++ src/components/loadMoreBtn/LoadMoreBtn.jsx
@@ -1,10 +1,9 @@
 import { Button } from "react-bootstrap";
 
-
 const LoadMoreBtn = ({ onClick }) => {
   return (
     <Button className='buttons' onClick={onClick}>
-      Load more
+      <span className="buttonText">Load more</span>
     </Button>
   );
 };
